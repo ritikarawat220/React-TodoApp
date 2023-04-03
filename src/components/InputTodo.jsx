59743cbd8e09260dbd08319c57dced142239dc83
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-03 15:23:54 +0530

    Add InputTodo component

---
+++ src/components/InputTodo.jsx
@@ -0,0 +1,10 @@
+const InputTodo = () => {
+    return (
+      <form>
+        <input type="text" placeholder="Add Todo..." />
+        <button>Submit</button>
+      </form>
+    );
+  };
+  export default InputTodo;
+  
