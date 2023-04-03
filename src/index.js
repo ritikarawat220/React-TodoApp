48559bba55bc1987ee25d6c25a1d1cf122781e97
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-03 17:31:11 +0530

    Fix linter error

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
 import TodoContainer from './components/TodoContainer';
 import './App.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <TodoContainer />
-    </Router>
+    <TodoContainer />
   </React.StrictMode>,
   document.getElementById('root'),
 );
