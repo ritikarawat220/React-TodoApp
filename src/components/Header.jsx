bf7b2f8b27ee737ddb12fc6f1e21cc5fcc590300
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-03 15:47:19 +0530

    Add styling in header.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+const Header = () => {
+    const headerStyle = {
+        padding: '20px 0',
+        lineHeight: '1.5em',
+        color: '#aeadad',
+        textAlign: 'center',
+      };
+      return (
+        <header style={headerStyle}>
+        style={{
+          padding: '20px 0',
+          lineHeight: '1.5em',
+          color: '#aeadad',
+          textAlign: 'center',
+        }}
+        {/* ... */}
+      </header>
+    );
+  };
+  export default Header;
+  
