e9674d23cd9e2a5ec827761805ad1a73a452b8df
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-03 15:43:46 +0530

    Add TodoApp component and update input component

--- src/components/InputTodo.jsx
+++ src/components/InputTodo.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
+  const [message, setMessage] = useState('');
 
   const handleChange = (e) => {
     setTitle(e.target.value);
@@ -8,20 +9,29 @@ const InputTodo = ({ addTodoItem }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    addTodoItem(title);
-    setTitle('');
+    if (title.trim()) {
+        addTodoItem(title);
+        setTitle('');
+        setMessage('');
+    } else {
+      setMessage('Please add item.');
+      }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <>
+   <form onSubmit={handleSubmit} className="form-container">
       <input
         type="text"
         placeholder="Add Todo..."
         value={title}
         onChange={handleChange}
+        className="input-text"
       />
-      <button>Submit</button>
+      <button className="input-submit">Submit</button>
     </form>
+    <span className="submit-warning">{message}</span>
+    </>
   );
 };
 export default InputTodo;
