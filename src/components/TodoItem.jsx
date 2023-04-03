cf4cb2162586ae772790385904b689eab8e7d459
Ritika Rawat <Ritikarawat220@gmail.com>
2023-04-03 15:06:58 +0530

    Add handleChange handler with the id

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,16 @@
 import { useState , useEffect } from 'react';
 const TodoItem = ({ itemProp, setTodos }) => {
+    
+    const handleChange = (id) => {
+        console.log('clicked', id);
+      };
+
     return (
       <li>
         <input
           type="checkbox"
           checked={itemProp.completed}
-          onChange={() => console.log('clicked')}
+          onChange={() => handleChange(itemProp.id)}
         />
         {itemProp.title}
       </li>
